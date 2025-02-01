fca52489d69eca7fe1fc52ba315a1682daf74d6d
ssb-shree <bavachikarshree1666@gmail.com>
2025-02-01 09:08:38 +0530

    add tailwind-v4 daisyui-beta routes configured and created an axios instance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Homepage from './pages/Homepage'
 
+import UserProfile from './pages/UserProfile'
+import Settings from './pages/Settings'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path={"/"} element={<Homepage/>} />
+        <Route path={"/register"} element={<Register/>} />
+        <Route path={"/login"} element={<Login/>} />
+        <Route path={"/profile"} element={<UserProfile/>} />
+        <Route path={"/settings"} element={<Settings/>} />
+      </Routes>
+    </div>
   )
 }
 
